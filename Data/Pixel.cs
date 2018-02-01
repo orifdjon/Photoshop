de054e55410191188e0807aa2140ffe8ec2ea716
Arif <orif95@mail.ru>
2018-02-02 01:19:25 +0300

    init _Photoshop

---
+++ Data/Pixel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.CodeDom;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+
+namespace MyPhotoshop
+{
+    public struct Pixel
+    {
+        public Pixel(double r, double g, double b)
+        {
+            _r = _g = _b = 0;
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+
+        private static double Check(double value)
+        {
+            if (value < 0 || value > 1) throw new ArgumentException();
+            return value;
+        }
+
+        internal static double Trim(double value)
+        {
+            if (value > 1) return value = 1;
+            if (value < 0) return value = 0;
+            return value;
+        }
+
+        private double _r;
+
+        public double R
+        {
+            get => _r;
+            set => _r = Check(value);
+        }
+
+        private double _g;
+
+        public double G
+        {
+            get => _g;
+            set => _g = Check(value);
+        }
+
+        private double _b;
+
+        public double B
+        {
+            get => _b;
+            set => _b = Check(value);
+        }
+
+        public static Pixel operator *(Pixel pixel, double value)
+        {
+            return new Pixel(
+                Pixel.Trim(pixel.R * value),
+                Pixel.Trim(pixel.G * value),
+                Pixel.Trim(pixel.B * value)
+            );
+        }
+        
+        public static Pixel operator *(double value, Pixel pixel)
+        {
+            return new Pixel(
+                Pixel.Trim(pixel.R * value),
+                Pixel.Trim(pixel.G * value),
+                Pixel.Trim(pixel.B * value)
+            );
+        }
+    }
+}
---
+++ Filters/GrayscaleFilter.cs
@@ -0,0 +1,31 @@
+﻿namespace MyPhotoshop
+{
+    public class GrayscaleFilter : IFilter
+    {
+        public ParameterInfo[] GetParameters()
+        {
+            return new ParameterInfo[0];
+        }
+
+        public override string ToString()
+        {
+            return "Оттенки серого.";
+        }
+
+
+        public Photo Process(Photo original, double[] parameters)
+        {
+            var result = new Photo(original.width, original.height);
+            for (var x = 0; x < result.width; x++)
+            {
+                for (var y = 0; y < result.height; y++)
+                {
+                    var lightness = (original[x, y].R + original[x, y].G + original[x, y].B) / 3;
+                    result[x, y] = new Pixel(lightness, lightness, lightness);
+                }
+            }
+
+            return result;
+        }
+    }
+}
